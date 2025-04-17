1c2f600eab4f44202c476db0222b5110920a7637
zmardoc <mol1@seznam.com>
2025-04-17 22:55:31 +0200

    fix e2e test

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -6,15 +6,14 @@ test('login and logout flow', async ({ page }) => {
   await page.click('button:has-text("Login")')
 
   await expect(page).toHaveURL('fidoo/#/welcome')
-  await expect(page.locator('text=Welcome testuser@fidoo.com')).toBeVisible()
+  await expect(page.locator('text=Welcome, testuser@fidoo.com')).toBeVisible()
   await expect(
     page.locator('a', { hasText: 'testuser@fidoo.com' }),
   ).toBeVisible()
 
-  const logoutLink = page.locator('button:has-text("logout")')
-  await logoutLink.waitFor({ state: 'visible', timeout: 7000 })
+  const logoutLink = page.locator('button:has-text("log out")')
+  await logoutLink.waitFor({ state: 'visible', timeout: 8000 })
 
-  //
   await logoutLink.click()
 
   await expect(page).toHaveURL('fidoo/#/')
