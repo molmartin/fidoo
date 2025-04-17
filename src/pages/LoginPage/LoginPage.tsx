6fb5335aadff2e30958ea35464f123b53ea0e3ec
zmardoc <mol1@seznam.com>
2025-04-17 19:22:21 +0200

    login form

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,42 +1,14 @@
 import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import {
-  Box,
-  Button,
-  Container,
-  TextField,
-  CircularProgress,
-  Snackbar,
-} from '@mui/material'
-import useLogin from './hooks/useLogin'
-import useLoginSession from '../../hooks/useLoginSession'
+import { Container, Snackbar } from '@mui/material'
+import LoginForm from './components/LoginForm'
 
 function LoginPage() {
-  const [username, setUsername] = useState('')
   const navigate = useNavigate()
-  const { mutate: login, isError, error, isPending } = useLogin()
-  const { setSession } = useLoginSession()
-
   const location = useLocation()
   const wasKicked = location.state?.fromProtected === true
 
   const [snackbarOpen, setSnackbarOpen] = useState(false)
-
-  function handleClose(): void {
-    setSnackbarOpen(false)
-  }
-
-  function handleLogin(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    login(username, {
-      onSuccess: (response) => {
-        if (!response.error) {
-          setSession(response.data)
-          navigate('/welcome')
-        }
-      },
-    })
-  }
 
   useEffect(() => {
     if (wasKicked) {
@@ -45,41 +17,13 @@ function LoginPage() {
     }
   }, [wasKicked, location.pathname, navigate])
 
+  function handleClose(): void {
+    setSnackbarOpen(false)
+  }
+
   return (
     <Container maxWidth="sm">
-      <Box
-        component="form"
-        autoComplete="off"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        height="100dvh"
-        paddingX={6}
-        gap={2}
-        onSubmit={handleLogin}
-      >
-        <TextField
-          label="Type your username"
-          slotProps={{
-            htmlInput: {
-              'data-tid': 'input-username',
-            },
-          }}
-          value={username}
-          error={isError}
-          helperText={isError && error instanceof Error ? error.message : ' '}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          size="large"
-          sx={{ height: '50px' }}
-          disabled={isPending || username.trim() === ''}
-        >
-          {isPending ? <CircularProgress size={24} color="inherit" /> : 'Login'}
-        </Button>
-      </Box>
+      <LoginForm onLoginSuccess={() => navigate('/welcome')} />
       <Snackbar
         open={snackbarOpen}
         autoHideDuration={3000}
---
+++ src/pages/LoginPage/components/LoginForm.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import { Box, Button, CircularProgress, TextField } from '@mui/material'
+import useLogin from '../hooks/useLogin'
+import useLoginSession from '../../../hooks/useLoginSession'
+
+type LoginFormProps = {
+  onLoginSuccess: () => void
+}
+
+function LoginForm({ onLoginSuccess }: LoginFormProps) {
+  const [username, setUsername] = useState('')
+  const { mutate: login, isError, error, isPending } = useLogin()
+  const { setSession } = useLoginSession()
+
+  function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    login(username, {
+      onSuccess: (response) => {
+        if (!response.error) {
+          setSession(response.data)
+          onLoginSuccess()
+        }
+      },
+    })
+  }
+
+  return (
+    <Box
+      component="form"
+      autoComplete="off"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      height="100dvh"
+      paddingX={6}
+      gap={2}
+      onSubmit={handleLogin}
+    >
+      <TextField
+        label="Type your username"
+        slotProps={{
+          htmlInput: {
+            'data-tid': 'input-username',
+          },
+        }}
+        value={username}
+        error={isError}
+        helperText={isError && error instanceof Error ? error.message : ' '}
+        onChange={({ target }) => setUsername(target.value)}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        size="large"
+        sx={{ height: '56px' }}
+        disabled={isPending || username.trim() === ''}
+      >
+        {isPending ? <CircularProgress size={24} color="inherit" /> : 'Login'}
+      </Button>
+    </Box>
+  )
+}
+
+export default LoginForm
